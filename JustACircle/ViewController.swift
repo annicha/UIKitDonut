50e5e89dfdfc55463034c6083f4ab0c67cb5bbb9
Anne <annicha.dev@gmail.com>
2020-03-22 23:34:33 +0700

    Initial Commit

---
+++ JustACircle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JustACircle
+//
+//  Created by Anne on 22/3/20.
+//  Copyright © 2020 Anne Hanw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
